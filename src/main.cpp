877127a709bf5c7cbd65073aed70373c276b76bd
Udi Lumnitz <u.lumnitz@applicaster.com>
2020-05-23 21:25:12 +0300

    adding stopFinger and servoCalibration

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,62 @@
 #include <Arduino.h>
 #include <Coordinates.h>
+#include <Servo.h>
+#include <PID_v1.h>
 
-int ang = 1;
-int dist = 10;
+Servo fingerOne;
+int analogPin = A5;
+int valAng;
 
-void getFingerPoint(float r, float phi) {
+float *getFingerPoint(float r, float phi)
+{
+  // This method is doing coordinates conversion from polar, r = distance and phi = angle
   Coordinates point = Coordinates();
-  point.fromPolar(r,phi);
-  Serial.println(point.getX());
-  Serial.print(", ");
-  Serial.println(point.getY());
+  point.fromPolar(r, phi);
+  float myPoint[] = {point.getX(), point.getY()};
+  return myPoint;
 }
 
-void setup() {
+int stopFinger(float ang)
+{
+  if (ang >= 500)
+  {
+    fingerOne.attach(8);
+    fingerOne.write(500);
+  }
+  else
+  {
+    fingerOne.detach();
+  }
+}
+
+int servoCalibration()
+{
+  fingerOne.attach(8);
+  for (size_t i = 0; i < 2400; i++)
+  {
+    fingerOne.write(i);
+    delay(100);
+    valAng = analogRead(analogPin);
+    Serial.print(i);
+    Serial.print(" ");
+    Serial.print(valAng);
+    Serial.println("");
+  }
+}
+
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(analogPin, INPUT);
+  servoCalibration();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  delay(100);
-  Serial.println("testing coordinates conversion from polar => ");
-  getFingerPoint(dist, ang);
-  delay(1000);
-  Serial.print("\n\n\n");
-  ang = ang + 3;
-  dist = dist + 5;
-} 
+  //valAng = analogRead(analogPin);
+  //Serial.println(valAng);
+  //stopFinger(valAng);
+  delay(15); // waits for the servo to get there
+}
