2e9d6f5db69d2fe551041f3282a7b27f0ab5a2f1
Udi Lumnitz <u.lumnitz@applicaster.com>
2020-07-02 10:53:50 +0300

    handling unity input and adding 3 finger

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,17 @@
 
 Servo servoOne;
 Servo servoTwo;
-int analogPin1 = A5;
-int analogPin2 = A0;
-int valAng;
-int servoOneAngles[537];
-int servoTwoAngles[537];
+Servo servoThree;
+
+int analogPin1 = A2;
+int inputPin1 = 8;
+int analogPin2 = A1;
+int inputPin2 = 7;
+int analogPin3 = A3;
+int inputPin3 = 6;
+
+int ledPin = 13;
+int virtualLimitNum = 100;
 
 float *getFingerPoint(float r, float phi)
 {
@@ -20,14 +26,17 @@ float *getFingerPoint(float r, float phi)
   return myPoint;
 }
 
-int stopFingerOne(int ang)
+int stopFingerOne(int virtualPoint1)
 {
-  if (ang >= 500)
+  int currAng1 = analogRead(analogPin1);
+
+  if (virtualPoint1 <= virtualLimitNum)
   {
-    servoOne.attach(8);
+    int writeVal = map(virtualPoint1, 0, virtualLimitNum, 23, 183);
+    servoOne.attach(inputPin1);
     servoOne.writeMicroseconds(0.5);
-    servoOne.write(servoOneAngles[ang]);
-    delay(1000);
+    servoOne.write(writeVal);
+    delay(100);
   }
   else
   {
@@ -35,14 +44,17 @@ int stopFingerOne(int ang)
   }
 }
 
-int stopFingerTwo(int ang)
+int stopFingerTwo(int virtualPoint2)
 {
-  if (ang >= 500)
+  int currAng2 = analogRead(analogPin2);
+
+  if (virtualPoint2 <= virtualLimitNum)
   {
-    servoTwo.attach(4);
+    int writeVal = map(virtualPoint2, 0, virtualLimitNum, 23, 183);
+    servoTwo.attach(inputPin2);
     servoTwo.writeMicroseconds(0.5);
-    servoTwo.write(servoTwoAngles[ang]);
-    delay(1000);
+    servoTwo.write(writeVal);
+    delay(100);
   }
   else
   {
@@ -50,53 +62,70 @@ int stopFingerTwo(int ang)
   }
 }
 
-int initCalibrationOfServos()
+int stopFingerThree(int virtualPoint3)
 {
-  servoOne.attach(8);
-  servoOne.write(0);
-  servoTwo.attach(4);
-  servoTwo.write(0);
-  delay(150);
+  int currAng3 = analogRead(analogPin3);
 
-  for (size_t i = 536; i < 2418; i++)
+  if (virtualPoint3 <= virtualLimitNum)
   {
-    servoOne.write(i);
-    servoTwo.write(i);
-    delay(150);
-    
-    int currAng1 = analogRead(analogPin1);
-    int currAng2 = analogRead(analogPin2);
-    
-    servoOneAngles[currAng1] = i;
-    Serial.print("save into servoOneAngles index: ");
-    Serial.print(currAng1);
-    Serial.print(" value = ");
-    Serial.print(servoOneAngles[currAng1]);
-    Serial.println("");
-
-    servoTwoAngles[currAng2] = i;
-    Serial.print("save into servoTwoAngles index: ");
-    Serial.print(currAng2);
-    Serial.print(" value = ");
-    Serial.print(servoTwoAngles[currAng2]);
-    Serial.println("");
+    int writeVal = map(virtualPoint3, 0, virtualLimitNum, 23, 183);
+    servoThree.attach(inputPin3);
+    servoThree.writeMicroseconds(0.5);
+    servoThree.write(writeVal);
+    delay(100);
   }
-  servoOne.detach();
-  servoTwo.detach();
+  else
+  {
+    servoThree.detach();
+  }
+}
+
+void handleUnityVirtualObject()
+{
+  if (Serial.available() > 0)
+  {
+    String incomingData = Serial.readStringUntil('\n');
+
+    if (incomingData.startsWith("data"))
+    {
+      String mesg;
+      int startPos = incomingData.indexOf(',');
+      String Value = incomingData.substring(startPos + 1);
+      startPos = Value.indexOf(',');
+      String Value1 = Value.substring(0, startPos);
+      Value = Value.substring(startPos + 1);
+      startPos = Value.indexOf(',');
+      String Value2 = Value.substring(0, startPos);
+      String Value3 = Value.substring(startPos + 1);
+
+      stopFingerOne(Value1.toInt());
+      stopFingerTwo(Value2.toInt());
+      stopFingerThree(Value3.toInt());
+
+      // for testing
+      if (Value1.toInt() == 10000 && Value2.toInt() == 10000 && Value3.toInt() == 10000)
+      {
+        for (int i = 0; i < 10; i++)
+        {
+          digitalWrite(ledPin, HIGH);
+          delay(100);
+          digitalWrite(ledPin, LOW);
+          delay(100);
+        }
+      }
+    }
+  }
 }
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(analogPin1, INPUT);
   pinMode(analogPin2, INPUT);
-  initCalibrationOfServos();
+  pinMode(analogPin3, INPUT);
 }
 
 void loop()
 {
-  int currAng1 = analogRead(analogPin1);
-  int currAng2 = analogRead(analogPin2);
-  stopFingerOne(currAng1);
-  stopFingerTwo(currAng2);
+  handleUnityVirtualObject();  
 }
